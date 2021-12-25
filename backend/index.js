3f87f8490ddbee034fd57182f348b78d6929e06c
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-25 21:04:24 +0530

    Add custom error handler

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import './config/env.js';
 import connectDb from './database/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 connectDb();
 
 const port = process.env.PORT;
 
 const app = express();
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.send("hello");
