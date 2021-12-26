33875a7ae218bfb6f9922a6ae22ee9401fba56bb
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-26 13:56:09 +0530

    Setup Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
+  <Provider store={store}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Provider>,
   document.getElementById('root')
 );
