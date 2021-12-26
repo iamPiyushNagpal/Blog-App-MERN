c3d75132e4d809c68677449be9ee0da7d0bd9270
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-27 01:51:33 +0530

    Add Poppins Font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import store from './store';
 
+const theme = extendTheme({
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  }
+})
+
 ReactDOM.render(
   <Provider store={store}>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </Provider>,
