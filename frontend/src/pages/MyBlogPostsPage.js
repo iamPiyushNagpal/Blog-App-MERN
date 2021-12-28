e467b8b8119628857b6c009990f0a2f6ebc5b8e4
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-28 23:48:11 +0530

    Dispatch deleteBlogPost Action

--- frontend/src/pages/MyBlogPostsPage.js
+++ frontend/src/pages/MyBlogPostsPage.js
@@ -1,13 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getBlogPostsByAuthor } from '../actions/blogPostActions';
-import { Flex, Image, Heading, Text, Box, Button, Container } from '@chakra-ui/react';
+import { getBlogPostsByAuthor, deleteBlogPost } from '../actions/blogPostActions';
+import {
+    Flex, Image, Heading, Text, Box, Button, Container, HStack, AlertDialog,
+    AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent,
+    AlertDialogOverlay,
+} from '@chakra-ui/react';
 import MDEditor from '@uiw/react-md-editor';
 import Message from '../components/Message';
 import Spinner from '../components/Spinner';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 const MyBlogPostsPage = () => {
+
+    // Required for AlertDialog to work
+    const [isOpen, setIsOpen] = useState(false)
+    const onClose = () => setIsOpen(false)
+    const cancelRef = useRef()
 
     const dispatch = useDispatch();
 
@@ -17,12 +26,22 @@ const MyBlogPostsPage = () => {
     const blogPostListByAuthor = useSelector(state => state.blogPostListByAuthor);
     const { error, loading, blogPosts } = blogPostListByAuthor;
 
+    const blogPostDelete = useSelector(state => state.blogPostDelete);
+    const { error: errorDelete, loading: loadingDelete, success: successDelete } = blogPostDelete;
+
     useEffect(() => {
         dispatch(getBlogPostsByAuthor(userInfo._id));
-    }, [dispatch, userInfo._id])
+    }, [dispatch, userInfo._id, successDelete])
+
+    const deleteBlogPostHandler = (id) => {
+        dispatch(deleteBlogPost(id));
+        setIsOpen(false);
+    }
 
     return (
         <Container maxW={'container.xl'}>
+            {loadingDelete && <Spinner />}
+            {errorDelete && <Message status="error" description={error} />}
             {loading ? <Spinner /> : error ? <Message status="error" description={error} /> :
                 <Box>
                     {blogPosts.map((blogPost) => (
@@ -47,9 +66,50 @@ const MyBlogPostsPage = () => {
                                     colorScheme={'purple'}
                                     mt={2}
                                 >READ MORE</Button>
+                                <HStack mt={2}>
+                                    <Button
+                                        as={ReactRouterLink}
+                                        to={``}
+                                        colorScheme={'blue'}
+                                        rightIcon={<i className="fa-solid fa-pencil"></i>}
+                                    >EDIT</Button>
+                                    <Button
+                                        colorScheme={'red'}
+                                        rightIcon={<i className="fa-solid fa-trash"></i>}
+                                        onClick={() => setIsOpen(true)}
+                                    >DELETE</Button>
+                                </HStack>
                             </Box>
+                            <AlertDialog
+                                isOpen={isOpen}
+                                leastDestructiveRef={cancelRef}
+                                onClose={onClose}
+                            >
+                                <AlertDialogOverlay>
+                                    <AlertDialogContent>
+                                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                                            Delete Blog Post
+                                        </AlertDialogHeader>
+                                        <AlertDialogBody>
+                                            Are you sure? You can't undo this action afterwards.
+                                        </AlertDialogBody>
+                                        <AlertDialogFooter>
+                                            <Button
+                                                ref={cancelRef}
+                                                onClick={onClose}>
+                                                Cancel
+                                            </Button>
+                                            <Button
+                                                colorScheme='red'
+                                                onClick={() => deleteBlogPostHandler(blogPost._id)}
+                                                ml={3}>
+                                                Delete
+                                            </Button>
+                                        </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                </AlertDialogOverlay>
+                            </AlertDialog>
                         </Flex>
-
                     ))}
                 </Box>
             }
