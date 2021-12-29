d109738841929ece4718db0c797257b52fdeb842
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-29 19:52:46 +0530

    Prepare for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import './config/env.js';
 import connectDb from './database/db.js';
@@ -19,6 +20,14 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/blogs', blogPostRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
 
 app.use(notFound);
 app.use(errorHandler);
